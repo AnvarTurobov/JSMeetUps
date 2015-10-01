e7a045702b3395d27bb672fda3bfeb7882411913
AnvarTurobov <anvar.turobov@gmail.com>
2015-10-01 06:38:16 +0100

    updating dashboard edit

--- both/controllers/dashboard.js
+++ both/controllers/dashboard.js
@@ -11,7 +11,34 @@ DashboardController.helpers({
 });
 
 DashboardController.events({
-  'click [data-action=doSomething]': function (event, template) {
-    event.preventDefault();
+    'submit .update-meetup-form': function(event) {
+     var title      = event.target.title.value;
+     var email      = event.target.email.value;
+     var topics     = event.target.topics.value;
+     var type       = event.target.type.value;
+     var address    = event.target.address.value;
+     var city       = event.target.city.value;
+     var postcode   = event.target.postcode.value;
+     var meetupdate = event.target.meetupdate.value;
+     var id         = event.target.id.value;
+
+     var params = {
+      title: title,
+      email: email,
+      topics: topics,
+      type: type,
+      address: address,
+      city: city,
+      postcode: postcode,
+      meetupdate: meetupdate,
+      createdAt: new Date()
+    }
+
+    Meteor.call('addMeetup', params); //inserting meetup
+
+    toastr.success('Meetup Added');
+    Router.go('/meetups');
+
+    return false;
   }
 });
